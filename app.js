6aa113dfb9093cea0715c8246a1b34b8a1b12180
leechan <dlcks0601@gmail.com>
2024-10-08 23:19:18 +0900

    refactor : authController, app.js 리펙토링

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors'); // CORS 라이브러리 추가
 const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/users');
 const placeRoutes = require('./routes/place');
@@ -9,8 +10,13 @@ const pinRoutes = require('./routes/ping');
 const app = express();
 require('dotenv').config();
 
+// CORS 설정 추가
+app.use(cors()); // 모든 출처에서 오는 요청 허용
+
+// JSON 요청 처리 미들웨어
 app.use(express.json());
 
+// 라우트 설정
 app.use('/api', placeRoutes);
 app.use('/auth', authRoutes);
 app.use('/users', userRoutes);
--- config/db.js
+++ config/db.js
@@ -1,22 +1,23 @@
 const mysql = require('mysql2/promise');
 
 const db = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'root',
-  database: 'test',
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'Test', // 데이터베이스 이름
+    port: 3306,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
 });
 
 db.getConnection()
-  .then((connection) => {
-    console.log('Database connected successfully');
-    connection.release();
-  })
-  .catch((err) => {
-    console.error('Database connection failed:', err);
-  });
+    .then((connection) => {
+        console.log('Database connected successfully');
+        connection.release();
+    })
+    .catch((err) => {
+        console.error('Database connection failed:', err);
+    });
 
 module.exports = db;
